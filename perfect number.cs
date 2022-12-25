0900c54326a56098cc121a9f34f6d06310ecd26c
HananElhosiny <121315349+HananElhosiny@users.noreply.github.com>
2022-12-25 16:08:13 +0200

    Add files via upload

---
+++ perfect number.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace Dcoder
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       
+     int startingnumber,endingnumber,counter,z;
+    
+    
+    Console.WriteLine("enter the starting(smallest) number(positive number)");
+    startingnumber =int.Parse(Console.ReadLine());
+    
+    Console.WriteLine("enter the ending(biggest) number(positive number)");
+    endingnumber =int.Parse(Console.ReadLine());
+    
+    for( z=startingnumber; z<=endingnumber; z++)
+    {int sum=0;
+      for(counter=1;counter<z;counter++)
+    {
+    if(z%counter==0)
+    {
+      sum=sum+counter;
+    }
+    }
+    if(z==sum)
+    Console.WriteLine(z);
+    }
+    
+    }
+   }
+ }
